a8dcfa4f06cd29fc2b570178f7c1fedde03ea306
Ryanprg <112130408+Ryanprg@users.noreply.github.com>
2026-02-25 18:20:28 -0800

    Update app.js

--- public/static/app.js
+++ public/static/app.js
@@ -26,7 +26,7 @@ const authMeBtn = document.getElementById('auth-me-btn')
 const authMeResult = document.getElementById('auth-me-result')
 const authLogoutBtn = document.getElementById('auth-logout-btn')
 
-const AUTH_TOKEN_KEY = '0f13b03f-579f-418e-aa17-20c27f754098'
+const AUTH_TOKEN_KEY = 'a5b28747-5cfa-4231-9f6f-cf65f92ad4cf'
 
 const renderResult = (target, html) => {
   if (!target) return
